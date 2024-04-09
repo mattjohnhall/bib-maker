4e7d9e5014f00af95ec9ca07c95c37d819a81ead
Matt Hall <matt@blocdigital.col>
2024-04-10 00:38:58 +0100

    Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "London Marathon Bib Generator",
+  title: "London Marathon Bib Maker",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-gray-800`}>{children}</body>
+      <body className={`${inter.className} text-gray-800`}>
+        {children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
