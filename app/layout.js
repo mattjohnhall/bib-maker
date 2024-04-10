b09b5b98d5b43c3ccbe1f5c1d03c2a85c5ae208b
Matt Hall <matt@blocdigital.col>
2024-04-10 09:01:29 +0100

    Add social meta

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,43 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
-import "./globals.css";
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
+
+const meta = {
+  title: "London Marathon Bib Maker",
+  description: "Create a custom avatar of your London Marathon race bib",
+  image: `${process.env.NEXT_PUBLIC_URL}/api/bib?number=58336&start=blue`,
+}
 
 export const metadata = {
-  title: "London Marathon Bib Maker",
-  description: "",
-};
+  title: meta.title,
+  description: meta.description,
+  openGraph: {
+    title: meta.title,
+    description: meta.description,
+    url: process.env.NEXT_PUBLIC_URL,
+    siteName: meta.title,
+    images: [
+      {
+        url: meta.image,
+        width: 1024,
+        height: 1024
+      }
+    ],
+    locale: "en_GB",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: meta.title,
+    description: meta.description,
+    creator: "@madebymh",
+    images: [meta.image],
+  },
+}
+
+
 
 export default function RootLayout({ children }) {
   return (
